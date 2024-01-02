001042feb2867d934837bc652435538cc127d3ee
TomCas105 <137158062+TomCas105@users.noreply.github.com>
2024-01-02 11:18:02 +0100

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+class Tile {
+private:
+    int _type;
+    int _state;
+
+public:
+    Tile();
+    ~Tile();
+};
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
