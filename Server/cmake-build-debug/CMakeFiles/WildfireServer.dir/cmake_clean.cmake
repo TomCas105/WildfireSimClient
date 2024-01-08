45d3d5832c43280de9c6fe7895634a4c6ec7cc15
TomCas105 <137158062+TomCas105@users.noreply.github.com>
2024-01-08 20:56:50 +0100

    .

---
+++ Server/cmake-build-debug/CMakeFiles/WildfireServer.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/WildfireServer.dir/main.c.o"
+  "CMakeFiles/WildfireServer.dir/pos_sockets/active_socket.c.o"
+  "CMakeFiles/WildfireServer.dir/pos_sockets/char_buffer.c.o"
+  "CMakeFiles/WildfireServer.dir/pos_sockets/passive_socket.c.o"
+  "WildfireServer"
+  "WildfireServer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/WildfireServer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
